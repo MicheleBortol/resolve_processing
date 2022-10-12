1ed906f9d2eee4fe181caaa3535362744aa6e45b
Michele Bortolomeazzi <michele.bortolomeazzi@dkfz-heidelberg.de>
2022-10-12 10:39:36 +0200

    Add CLAHE to segmentation.

--- bin/segmenter.py
+++ bin/segmenter.py
@@ -3,7 +3,19 @@ import numpy as np
 import cv2
 from cellpose import models, io, utils
 from roifile import ImagejRoi, roiwrite
-from skimage import morphology, segmentation
+from skimage import exposure, morphology, segmentation
+
+def claher(img):
+	"""
+	Runs Contrast Limited Adaptive Histogram Equalization (CLAHE)
+	on the image and converts it to 8bit
+	"""
+	img = exposure.equalize_adapthist(img, kernel_size = 127, clip_limit = 0.01, nbins = 256)
+	img = img / img.max() #normalizes img in range 0 - 255
+	img = 255 * img
+	img = img.astype(np.uint8)
+	return img
+
 
 def trim(image):
 	"""
@@ -61,6 +73,9 @@ if __name__ == "__main__":
 		print("Loading the image.")
 		img = io.imread(tiff_path)
 		
+		# Perform Local Contrast Enhancment on the input image
+		img = claher(img)
+
 		# Apply the model
 		print("Segmenting cells.")
 		mask, flows, style, diameter = model.eval(img, diameter = cell_diameter,
@@ -81,9 +96,6 @@ if __name__ == "__main__":
 	
 		# extract outlines and make ROIs
 		if output_zip != None:
-			print("Setting up mask for extracting ROIs.")
-			trim(mask)
-			
 			print("Identifying ROIs.")
 			cell_number = np.amax(mask, axis = None) + 1
 
